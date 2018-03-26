394ac71b9485d93e09b4ffbf3c816e178d2a6645
Greg <leve0292@algonquinlive.com>
2018-03-26 12:17:24 -0400

    Added buttons for the different activities

--- app/src/main/java/com/example/greg/finalproject/StartActivity.java
+++ app/src/main/java/com/example/greg/finalproject/StartActivity.java
@@ -2,6 +2,7 @@ package com.example.greg.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button buttonGreg = (Button)findViewById(R.id.greg);
+        Button buttonHarleen = (Button)findViewById(R.id.harleen);
+        Button buttonTran = (Button)findViewById(R.id.tran);
+        Button buttonJason = (Button)findViewById(R.id.jason);
     }
 }
