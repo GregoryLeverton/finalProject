d4e2cafed8947527933d35f616218df832eebe9c
Jason Meredith <jason-meredith@live.com>
2018-04-02 12:40:50 -0400

    Milestone 2 -Added help screen and GB string locale -Added internet permission to AndroidManifest.xml

---
+++ app/src/main/java/com/example/greg/octranspo/octranspo/Trip.java
@@ -0,0 +1,14 @@
+package com.example.greg.octranspo.octranspo;
+
+/**
+ * Created by jason on 02/04/18.
+ */
+
+public class Trip {
+
+    // Next trip info
+    private double gpsLat, gpsLong;
+    private double speed;
+    private String startTime;
+    private String adjustScheduleTime;
+}
